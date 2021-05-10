67469f825e15baeb1f926bf5e8bc9e2ab5f370bf
Victor Campelo <victor_campelo@outlook.com>
2021-05-10 15:04:18 -0300

    feat: add summary and dashboard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GLobalStyle } from "./styles/global";
 
@@ -6,6 +7,7 @@ export function App() {
     <>
       {/* header é algo que vai ser repetido em todas as telas criadas */}
       <Header /> 
+      <Dashboard />
       <GLobalStyle />
     </>
   );
