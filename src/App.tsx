f4b22860289c8fda37d23590483378d59198a798
Victor Campelo <victor_campelo@outlook.com>
2021-05-11 11:22:20 -0300

    feat: create transactions modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GLobalStyle } from "./styles/global";
 import { createServer } from "miragejs";
+import Modal from "react-modal";
+import { useState } from "react";
 
 createServer({
   routes() {
@@ -22,12 +24,37 @@ createServer({
   },
 });
 
+// Acessibilidade. 
+// Explicita que o modal está por cima da div root
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsnewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsnewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsnewTransactionModalOpen(false);
+  }
+
   return (
     <>
       {/* header é algo que vai ser repetido em todas as telas criadas */}
-      <Header />
+      {/* Repasse de funções  */}
+      <Header onOpenNewTransactionNewModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      
+      {/* Modais são exibidos por cima de todo codigo */}
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
+
       <GLobalStyle />
     </>
   );
